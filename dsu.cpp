67d7532918e15f4ff8b6d3e17edd9b52b3f0d96b
ArushNandankar <arush.nandankar@students.iiit.ac.in>
2024-05-15 14:12:52 +0000

    dsu updated

--- dsu.cpp
+++ dsu.cpp
@@ -1,35 +1,31 @@
 class DSU {
 private:
-    int n;
-    vi rank, parent;
-    void makeSet() {
-        for (int i = 0; i < n; i++)
-            parent[i] = i;
+    vi parent;
+    vi siz;
+    void make_set(int v) {
+        parent[v] = v; siz[v] = 1;
     }
 public:
-    DSU(int n) {
-        rank.resize(n);
+    DSU (int n) {
         parent.resize(n);
-        this->n = n;
-        makeSet();
-    }
-    int find(int x) {
-        if (parent[x] != x)
-            parent[x] = find(parent[x]);
-        return parent[x];
-    }
-    void Union(int x, int y) {
-        int xset = find(x);
-        int yset = find(y);
-        if (xset == yset)
-            return;
-        else if (rank[xset] < rank[yset])
-            parent[xset] = yset;
-        else if (rank[xset] > rank[yset])
-            parent[yset] = xset;
-        else {
-            parent[yset] = xset;
-            rank[xset] = rank[xset] + 1;
+        siz.resize(n);
+        for (int i = 0; i < n; i++) {
+            make_set(i);
         }
+    }
+    int find_set(int v) {
+        return (v == parent[v]) ? v : parent[v] = find_set(parent[v]);
+    }
+    void union_sets(int a, int b) {
+        a = find_set(a);
+        b = find_set(b);
+        if (a == b) return;
+        if(siz[a] < siz[b]) swap(a, b);
+        parent[b] = a;
+        siz[a] += siz[b]; 
+        siz[b] = 0; 
+    }
+    int get_size(int v){
+        return siz[find_set(v)];
     }
 };
