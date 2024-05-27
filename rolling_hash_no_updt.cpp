b5ed2be4a7a8fa25838e32fb5a2dc6af540197cf
Arush Nandankar <arush.nandankar@students.iiit.ac.in>
2024-05-27 17:47:18 +0530

    rolling hash no updates

--- rolling_hash_no_updt.cpp
+++ rolling_hash_no_updt.cpp
@@ -2,7 +2,7 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 
-#define int int
+#define int long long
 #define float double
 #define all(x) (x).begin(), (x).end()
 #define pnl cout << "\n"
@@ -22,67 +22,67 @@ using vvi = vector<vector<int>>;
 using pii = pair<int, int>;
 using vpii = vector<pair<int, int>>;
 
-using T = array<int, 3>;
-const T MOD = {1000000007, 1000000009, 1000000021};
-const T p = {101, 211, 307};
-
-T operator+(T a, int x) { return {(a[0] + x) % MOD[0], (a[1] + x) % MOD[1], (a[2] + x) % x}; }
-T operator-(T a, int x) { return {(a[0] - x + MOD[0]) % MOD[0], (a[1] - x + MOD[1]) % MOD[1], (a[2] - x + MOD[2]) % MOD[2]}; }
-T operator*(T a, int x) { return {(int)((int)a[0] * x % MOD[0]), (int)((int)a[1] * x % MOD[1]), (int)((int)a[2] * x % MOD[2])}; }
-T operator+(T a, T x) { return {(a[0] + x[0]) % MOD[0], (a[1] + x[1]) % MOD[1], (a[2] + x[2]) % MOD[2]}; }
-T operator-(T a, T x) { return {(a[0] - x[0] + MOD[0]) % MOD[0], (a[1] - x[1] + MOD[1]) % MOD[1], (a[2] - x[2] + MOD[2]) % MOD[2]}; }
-T operator*(T a, T x) { return {(int)((int)a[0] * x[0] % MOD[0]), (int)((int)a[1] * x[1] % MOD[1]), (int)((int)a[2] * x[2] % MOD[2])}; }
-ostream &operator<<(ostream &os, T hash) { return os << "(" << hash[0] << ", " << hash[1] << ", " << hash[2] << ")"; }
-
-const int N = 4e6 + 9;
+const int N = 2e6 + 9;
 int power(int n, int k, const int mod) {
-	int ans = 1 % mod;
-	n %= mod;
-	if (n < 0)
-		n += mod;
-	while (k) {
-		if (k & 1)
-			ans = (int)ans * n % mod;
-		n = (int)n * n % mod;
-		k >>= 1;
-	}
-	return ans;
+    int ans = 1 % mod;
+    n %= mod;
+    if (n < 0) n += mod;
+    while (k) {
+        if (k & 1) ans = (int)ans * n % mod;
+        n = (int)n * n % mod;
+        k >>= 1;
+    }
+    return ans;
 }
-T pw[N], ipw[N];
+const int MOD1 = 127657753, MOD2 = 987654319;
+const int p1 = 137, p2 = 277;
+int ip1, ip2;
+pair<int, int> pw[N], ipw[N];
+bool touched = false;
 void prec() {
-    pw[0] = {1, 1, 1};
+    if (touched) return;
+    touched = true;
+    pw[0] = {1, 1};
     for (int i = 1; i < N; i++) {
-        pw[i] = pw[i - 1] * p;
+        pw[i].first = 1LL * pw[i - 1].first * p1 % MOD1;
+        pw[i].second = 1LL * pw[i - 1].second * p2 % MOD2;
     }
-    ipw[0] = {1, 1, 1};
-    T ip = {power(p[0], MOD[0] - 2, MOD[0]), power(p[1], MOD[1] - 2, MOD[1]), power(p[2], MOD[2] - 2, MOD[2])};
+    ip1 = power(p1, MOD1 - 2, MOD1);
+    ip2 = power(p2, MOD2 - 2, MOD2);
+    ipw[0] = {1, 1};
     for (int i = 1; i < N; i++) {
-        ipw[i] = ipw[i - 1] * ip;
+        ipw[i].first = 1LL * ipw[i - 1].first * ip1 % MOD1;
+        ipw[i].second = 1LL * ipw[i - 1].second * ip2 % MOD2;
     }
 }
-
 struct Hashing {
-    vector<T> h1; // forward hash
-    vector<T> h2; // backward hash
-    Hashing(string s) {
-        int n = s.size();
-        h1.resize(n + 1);
-        h2.resize(n + 2);
-        for (int i = 0; i < n; i++) {
-            h1[i + 1] = h1[i] * p + s[i];
+    int n;
+    string s;                  // 0 - indexed
+    vector<pair<int, int>> hs; // 1 - indexed
+    Hashing() {}
+    Hashing(string _s) {
+        prec();
+        n = _s.size();
+        s = _s;
+        hs.emplace_back(0, 0);
+        for (int i = 0; i < n; i++)
+        {
+            pair<int, int> p;
+            p.first = (hs[i].first + 1LL * pw[i].first * s[i] % MOD1) % MOD1;
+            p.second = (hs[i].second + 1LL * pw[i].second * s[i] % MOD2) % MOD2;
+            hs.push_back(p);
         }
-        for (int i = n - 1; i >= 0; i--) {
-            h2[i] = h2[i + 1] * p + s[i];
-        }
     }
-    T get_hash(int l, int r) {
-        return (h1[r] - h1[l] * pw[r - l]) + (h2[l] - h2[r] * pw[r - l]);
+    pair<int, int> get_hash(int l, int r) { 
+        ++l; ++r; // 1 - indexed
+        assert(1 <= l && l <= r && r <= n);
+        pair<int, int> ans;
+        ans.first = (hs[r].first - hs[l - 1].first + MOD1) * 1LL * ipw[l - 1].first % MOD1;
+        ans.second = (hs[r].second - hs[l - 1].second + MOD2) * 1LL * ipw[l - 1].second % MOD2;
+        return ans;
     }
-    T get_rev_hash(int l, int r) {
-        return (h2[r] - h2[l] * pw[r - l]) + (h1[l] - h1[r] * pw[r - l]);
-    }
-    bool is_palindrome(int l, int r) {
-        return (get_hash(l, r) == get_rev_hash(l, r));
+    pair<int, int> get_hash() {
+        return get_hash(0, n - 1);
     }
 };
 
